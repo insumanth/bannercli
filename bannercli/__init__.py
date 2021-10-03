0b513e35589c3338fead60fd1b2454d648627088
Sumanth <sumanthreddystar@gmail.com>
2021-10-03 02:24:21 +0000

    Made base class with init and display methods.
    Changed bannercli.py and __init__.py files.
    Tested and working

--- bannercli/__init__.py
+++ bannercli/__init__.py
@@ -3,3 +3,8 @@
 __author__ = """Sumanth"""
 __email__ = 'sumanthreddystar@gmail.com'
 __version__ = '0.1.0'
+
+
+# Imports
+
+from .bannercli import banner
--- bannercli/bannercli.py
+++ bannercli/bannercli.py
@@ -1,1 +1,14 @@
 """Main module."""
+
+class banner:
+    """ Base Class for Banner Text"""
+    text = None
+    size = 1
+
+    def __init__(self, text = None, size = 1):
+        self.text = text
+        self.size = size
+    
+
+    def display(self):
+        print(self.text)
